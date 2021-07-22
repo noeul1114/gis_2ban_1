1e5e1e3117dd13ec735d760ff5cd425a2c3574d4
noeul1114 <parkhyongsok@naver.com>
2021-07-22 17:47:00 +0900

    7/22 2ban class commit 3

--- gis_2ban_1/settings.py
+++ gis_2ban_1/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'accountapp'
+    'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
