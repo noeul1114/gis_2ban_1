72b3fffd22e28e6eee1087c28ad35a71356cf86c
noeul1114 <parkhyongsok@naver.com>
2021-08-04 11:37:06 +0900

    8/4 class 2ban commit 3

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -11,12 +11,14 @@ from articleapp.models import Article
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'articleapp/create.html'
 
     def form_valid(self, form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
 class ArticleDetailView(DetailView):
@@ -24,6 +26,15 @@ class ArticleDetailView(DetailView):
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
 
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
 
